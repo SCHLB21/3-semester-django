d506c8551dd8fc61e91488ff24d9903c8cfd93d2
Dmitriy <dmitryweazy@gmail.com>
2021-01-14 01:07:21 +0300

    Добавлен метод POST для REST API

--- interview_service/api/views.py
+++ interview_service/api/views.py
@@ -16,9 +16,41 @@ class EmployeesView(APIView):
         serializer = EmployeesSerializer(EmployeesApi, many=True)
         return Response({"Employees": serializer.data})
 
+    def post(self, request):
+        EmployeesApi = request.data.get('Employees')
+        serializer = EmployeesSerializer(data=EmployeesApi)
+        if serializer.is_valid(raise_exception=True):
+            employee_saved = serializer.save()
+        return Response({"success": "Employee '{}' created successfully".format(employee_saved.name)})
+
 
 class EmployersView(APIView):
     def get(self, request):
         EmployersApi = Employers.objects.all()
         serializer = EmployersSerializer(EmployersApi, many=True)
-        return Response({"Employees": serializer.data})
+        return Response({"Employers": serializer.data})
+
+    def post(self, request):
+        EmployersApi = request.data.get('Employers')
+        serializer = EmployersSerializer(data=EmployersApi)
+        if serializer.is_valid(raise_exception=True):
+            employer_saved = serializer.save()
+        return Response({"success": "Employer '{}' created successfully".format(employer_saved.employer_name)})
+# POST запрос для Employees
+# {
+#     "Employees":{
+#         "id": 6,
+#         "name": "Dima",
+#         "surname": "Komkov",
+#         "email": "komkovdddf@mail.ru"
+#     }
+# }
+
+# POST запрос для Employers
+# {
+#     "Employers":{
+#         "id": 3,
+#         "employer_name": "Apple",
+#         "email": "ktoonktoon@mail.ru"
+#     }
+# }
