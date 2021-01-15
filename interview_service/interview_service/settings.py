4238f02e52c015e8fe0108f611b3292a48f1fc62
Dmitriy <dmitryweazy@gmail.com>
2021-01-15 17:24:12 +0300

    static

--- interview_service/interview_service/settings.py
+++ interview_service/interview_service/settings.py
@@ -14,8 +14,8 @@ import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).parent.parent
-
+BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
