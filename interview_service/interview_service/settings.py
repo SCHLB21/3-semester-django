5174c08da770b0681b108c83dc7325bbd55ac432
Dmitriy <dmitryweazy@gmail.com>
2021-01-15 16:36:10 +0300

    static

--- interview_service/interview_service/settings.py
+++ interview_service/interview_service/settings.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
-import os.path import sys
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -25,7 +25,7 @@ SECRET_KEY = 'f)rid35msfe(=(h!mzw=_r$vqaqi3=4mt5w9r=z%&(zd!z!-+='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-project.std-924.ist.mospolytech.ru', ]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -120,5 +120,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 # IMPORT_EXPORT_USE_TRANSACTIONS = True
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
