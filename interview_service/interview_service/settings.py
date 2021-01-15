b2113f9b98256e7d45c8e3b27824c32025a53b76
Dmitriy <dmitryweazy@gmail.com>
2021-01-15 16:07:38 +0300

    static

--- interview_service/interview_service/settings.py
+++ interview_service/interview_service/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os.path import sys
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
+# PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -120,5 +120,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 # IMPORT_EXPORT_USE_TRANSACTIONS = True
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
